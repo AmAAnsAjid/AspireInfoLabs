6384275584f49aa29e5f4e874254d9709d1f2b3b
AmAAnsAjid <91804414+AmAAnsAjid@users.noreply.github.com>
2023-07-03 23:21:02 +0530

    Sorry forgot to add comments

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const userData = {
   username: 'john123',
   password: 'password123',
-  email: 'amaan20ucse017@mahindrauniveristy.edu.in',
+  email: 'emmanuelle.hickle59@ethereal.email',
   confirmPassword: 'password123',
   collegename: 'ABC College',
   firstname: 'John',
@@ -18,22 +18,23 @@ axios
   .catch((error) => {
     console.error(error);
   });
-  // const loginData = {
-  //   username: 'john123',
-  //   password: 'password123',
-  // };
+  const loginData = {
+    username: 'john123',
+    password: 'password123',
+    otp:'123456'
+  };
   
-  // axios
-  //   .post('http://localhost:5000/login', loginData)
-  //   .then((response) => {
-  //     const token = response.data.token;
-  //     console.log('Login successful');
-  //     console.log('Token:', token);
+  axios
+    .post('http://localhost:5000/login', loginData)
+    .then((response) => {
+      const token = response.data.token;
+      console.log('Login successful');
+      console.log('Token:', token);
   
-  //     // Store the token securely and include it in subsequent requests
-  //     // to protected routes as an "Authorization" header with the value "Bearer <token>"
-  //   })
-  //   .catch((error) => {
-  //     console.error('Login failed:', error);
-  //   });
+      // Store the token securely and include it in subsequent requests
+      // to protected routes as an "Authorization" header with the value "Bearer <token>"
+    })
+    .catch((error) => {
+      console.log('Login failed:', error);
+    });
 
--- backend/server.js
+++ backend/server.js
@@ -11,22 +11,46 @@ const server = http.createServer(app);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+/**
+ * Handles the login request.
+ * Validates the provided username, password, and OTP.
+ * If the credentials are valid, a token is generated and returned.
+ */
 app.post('/login', (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, otp } = req.body;
 
-  // Validate the username and password
-  if (username === 'john123' && password === 'password123') {
-    const token = jwt.sign({ username }, 'secret-key', { expiresIn: '1h' });
-    res.json({ token });
-  } else {
-    res.status(401).json({ message: 'Invalid username or password' });
+  try {
+    // Read the user data from the file
+    const fileData = fs.readFileSync('details.json');
+    const users = JSON.parse(fileData);
+
+    // Find the user with the provided username
+    const user = users.find((user) => user.username === username);
+
+    if (user && user.password === password && otp === '123456') {
+      const token = jwt.sign({ username }, 'secret-key', { expiresIn: '1h' });
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.log("Error while searching for username");
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
+/**
+ * Handles the protected route.
+ * Only allows access if the request contains a valid token.
+ */
 app.get('/protected', authenticateToken, (req, res) => {
   res.json({ message: 'Protected route accessed successfully' });
 });
 
+/**
+ * Middleware function to authenticate the token in the request headers.
+ * If the token is valid, the user information is added to the request object.
+ */
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -45,6 +69,11 @@ function authenticateToken(req, res, next) {
   });
 }
 
+/**
+ * Handles the registration request.
+ * Saves the user data to the 'details.json' file.
+ * Generates an OTP and sends it to the registered email.
+ */
 app.post('/register', async (req, res) => {
   try {
     const userData = req.body; // Assuming req.body contains the user data
@@ -64,10 +93,12 @@ app.post('/register', async (req, res) => {
 
     // Write the updated data back to the file
     fs.writeFileSync('details.json', JSON.stringify(existingData, null, 2));
-		const otp = generateOTP(); // You can implement your own OTP generation logic
-    
+
+    const otp = generateOTP(); // You can implement your own OTP generation logic
+
     // Send OTP via email
     sendOTP(userData.email, otp); // Pass the registered email and OTP to the sendOTP function
+
     res.json({ message: 'Registration successful' });
   } catch (error) {
     console.error(error);
@@ -75,30 +106,29 @@ app.post('/register', async (req, res) => {
   }
 });
 
-
-
-
-server.listen(5000, () => {
-  console.log("Server started on port 8080");
-});
+/**
+ * Generates a random OTP.
+ * Replace with your own OTP generation logic.
+ */
 function generateOTP() {
   // Generate OTP logic here
   return '123456'; // Replace with your OTP generation code
 }
 
-// Function to send OTP via email
+/**
+ * Sends the OTP to the registered email.
+ * Uses nodemailer to send the email.
+ * Replace the email credentials and settings with your own.
+ */
 function sendOTP(email, otp) {
   // Create a nodemailer transporter
-	let testaccount =nodemailer.createTestAccount();
-	//connecting with 
-
   const transporter = nodemailer.createTransport({
-    host:"smtp.ethereal.email", 
-		port:587,// Replace with your email service provider (e.g., Gmail, Outlook)
+    host: "smtp.ethereal.email",
+    port: 587, // Replace with your email service provider (e.g., Gmail, Outlook)
     auth: {
-			user: 'emmanuelle.hickle59@ethereal.email',
-			pass: 'cRae6mme2VC8s2YRz8'
-	}
+      user: 'emmanuelle.hickle59@ethereal.email',
+      pass: 'cRae6mme2VC8s2YRz8'
+    }
   });
 
   // Configure the email options
@@ -118,3 +148,8 @@ function sendOTP(email, otp) {
     }
   });
 }
+
+// Start the server
+server.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
